caab8d7c39e53ce69f9e1adea5cd78c9bc19e3df
Khaled Makkawirelang <121877660+oledunwahid@users.noreply.github.com>
2023-07-31 14:57:39 +0700

    background-commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <>
+    <section className='h-[700px] flex items-center overflow-x-hidden bg-gradient-to-tr from-stone-300 via-red-300 to-white'></section>
+    <div>
+
+    </div>
+    </>
+  )
+}
+
+export default LandingPage
