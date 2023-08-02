cb7ab2632cf4747789997e3eb5bed09879dd4573
Khaled Makkawirelang <121877660+oledunwahid@users.noreply.github.com>
2023-08-02 10:32:14 +0700

    asset

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function LandingPage() {
   return (
     <>
-    <section className='h-[700px] flex items-center overflow-x-hidden bg-gradient-to-tr from-stone-300 via-red-300 to-white'>
-      <div>
+      <section className="h-[700px] flex items-center overflow-x-hidden bg-gradient-to-tr from-stone-300 via-red-300 to-white">
         <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row items-center lg:gap-y-8 lg:gap-x-56 text-center">
-          hello
+          <div className="flex flex-col lg:flex-row items-center justify-center lg:gap-y-8 lg:gap-x-56 h-full">
+            {/* text */}
+            <div className="text-center">
+              <h1 className="text-6xl text-white drop-shadow-md shadow-black-600/50">
+                Welcome <span /> to floracare
+              </h1>
+            </div>
+            
+          </div>
         </div>
-        </div>
-      </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
