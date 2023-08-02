41260a4ca392a42d2f83fa4dc56a519eb84396a2
ariiq <ariiqyusufdu@gmail.com>
2023-08-02 09:18:15 +0700

    import tailwind to src/index.css

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../components/NavBar";
+
+const LandingPage = () => {
+    return (
+        <Navbar></Navbar>
+    )
+}
+
+export default LandingPage;
